82f37000b0974bf57e32380f6df2894bc7aeb5d9
Sam Kuhlmann <samkuhlmann@gmail.com>
2014-11-12 10:04:40 -0700

    Added company name validation

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Company, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:company) {Company.new(name: "Burger King")}
+
+  it 'is valid' do
+    expect(company).to be_valid
+  end
+
+  it 'is invalid without a name' do 
+    company.name = nil
+    expect(company).not_to be_valid
+  end
+
 end
