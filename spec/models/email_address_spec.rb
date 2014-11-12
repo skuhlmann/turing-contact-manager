66e3d83ca855acabfd1515b67d8a3159c19cfaa1
Sam Kuhlmann <samkuhlmann@gmail.com>
2014-11-12 07:34:14 -0700

    Added email address attribute validation

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe EmailAddress, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:email_address) {EmailAddress.new(address: "sam@test.com", person_id: 2)}
+
+  it "is valid" do
+    expect(email_address).to be_valid
+  end
+
+  it "is invalid without an email" do 
+    email_address.address = nil
+    expect(email_address).to_not be_valid
+  end
+
+  it "must have a reference to a person" do 
+    email_address.person_id = nil
+    expect(email_address).to_not be_valid
+  end
 end
